fc3f7212861d3567a8f418b619ec201b9d5c1c98
heyueguo <heyueguo@goatgames.com>
2022-10-28 23:53:55 +0800

    创建登录路由

--- vue3test/vue3AdminSys/admin_sys/src/App.vue
+++ vue3test/vue3AdminSys/admin_sys/src/App.vue
@@ -1,35 +1,19 @@
 <template>
-
-  <el-button type="primary">Primary</el-button>
   
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  
-<!--  router 所显示的位置， 所以 使用 url/login 进入网页可以看到在 home 和 about 下有一个登录页面-->
+<!--  router 所显示的位置-->
   <router-view/> 
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+//默认的外内边距
+*{
+  padding: 0px;
+  margin: 0px;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+// 定义 标签和id的宽高属性
+html,body,#app{
+  width: 100%;
+  height: 100%;
 }
 </style>
--- vue3test/vue3AdminSys/admin_sys/src/views/LoginView.vue
+++ vue3test/vue3AdminSys/admin_sys/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 ﻿
 <template>
-  <div>
-    登录页面
+<!--  div 定义一个class，通过class设置div属性，和背景-->
+  <div class="login-box">
   </div>
 </template>
 
@@ -15,6 +15,11 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-
+<!--css预处理语言 scss-->
+<style lang="scss" scoped>
+  .login-box{
+    width: 100%;
+    height: 100%;
+    background: url("../assets/bg.jpg");
+  }
 </style>
