a4668a3ac92a31e36173fe69cab0b6453ce3cb5d
heyueguo <heyueguo@goatgames.com>
2022-10-30 20:49:36 +0800

    用户列表数据类型

---
+++ vue3test/vue3AdminSys/admin_sys/src/type/user.ts
@@ -0,0 +1,32 @@
+﻿
+export interface ListInt {
+    id:number
+    nickName:string
+    role:RoleInt[]
+    userName:string
+}
+
+interface RoleInt{
+    role:number
+    roleName:string
+}
+
+interface SelectDataInt{
+    role:number
+    nickName:string
+}
+
+interface RoleListInt{
+    authority:number[]
+    roleID:number
+    roleName:string
+}
+
+export class InitData{
+    selectData:SelectDataInt = {
+        nickName:"",
+        role:0
+    }
+    list:ListInt[] = []
+    roleList:RoleListInt[]=[]
+}
